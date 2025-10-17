d5adae6e5f433c5cd935322f2d359c6e05671191
Filippo Corti <filippo.corti1@studenti.unimi.it>
2025-10-17 09:40:28 +0200

    rewriting code

--- utils/bow.py
+++ utils/bow.py
@@ -11,19 +11,21 @@ class Bow:
     def __init__(
             self,
             corpus: List[list],
-            min_occurrences: int = 0,
-            max_occurrences: int = 500
+            min_occurrences: int = 0,  # Min number of occurrences to be in the BoW
+            max_occurrences: int = 500  # Max number of occurrences to be in the BoW
     ):
         self.corpus = corpus
-        self.min_occurrences = min_occurrences
-        self.max_occurrences = max_occurrences
+
+        # Build the BoW, counting word frequencies
         self.vocabulary = defaultdict(lambda: 0)
         for words in self.corpus:
             for word in words:
                 self.vocabulary[word] += 1
-        # filter
-        self.vocabulary = [x for x, y in self.vocabulary.items() if
-                           self.min_occurrences < y < self.max_occurrences]
+        self.vocabulary = [
+            x
+            for x, y in self.vocabulary.items()
+            if min_occurrences < y < max_occurrences
+        ]
 
         # Words are indexed (for efficiency)
         self.word2idx = dict([(w, i) for i, w in enumerate(self.vocabulary)])
@@ -34,11 +36,12 @@ class Bow:
         return len(self.vocabulary)
 
     def __getitem__(self, word):
+        """Returns the index of the given word"""
         return self.word2idx[word]
 
     def one_hot_skip_gram_dataloader(
             self,
-            window: int, # How many words before and after the target are in its context
+            window: int,  # How many words before and after the target are in its context
             batch: int = 4,
             shuffle: bool = False
     ) -> tuple[DataLoader, torch.Tensor, torch.Tensor]:
@@ -51,21 +54,34 @@ class Bow:
             batch (int): batch size
             shuffle (bool): if shuffle
         """
-        training_set_skip_gram_input = []
-        training_set_skip_gram_labels = []
+        skipgram_inputs = []
+        skipgram_outputs = []
         for words in self.corpus:
             for i, key in enumerate(words):
-                if key in self.word2idx.keys():
-                    key_vec = np.zeros(len(self.vocabulary))
-                    key_vec[self[key]] = 1
-                    target_vec = np.zeros(self.size)
-                    for target in words[max(0, i - window):i + window + 1]:
-                        if target in self.word2idx.keys() and key != target:
-                            target_vec[self[target]] = 1
-                    training_set_skip_gram_input.append(key_vec)
-                    training_set_skip_gram_labels.append(target_vec)
-        inputs = torch.Tensor(np.array(training_set_skip_gram_input))
-        outputs = torch.Tensor(np.array(training_set_skip_gram_labels))
+
+                # Skip central words which are not in the BoW
+                if key not in self.word2idx:
+                    continue
+
+                # Build the input vector of the word (only one "1")
+                key_vec = np.zeros(len(self.vocabulary))
+                key_vec[self[key]] = 1
+
+                # Build the target vector of the word ("1" for each context word)
+                target_vec = np.zeros(self.size)
+                context = words[max(0, i - window):i + window + 1]
+                for context_word in context:
+
+                    if key == context_word: continue
+                    if context_word not in self.word2idx: continue
+
+                    target_vec[self[context_word]] = 1
+
+                skipgram_inputs.append(key_vec)
+                skipgram_outputs.append(target_vec)
+
+        inputs = torch.Tensor(np.array(skipgram_inputs))
+        outputs = torch.Tensor(np.array(skipgram_outputs))
         dataset = TensorDataset(inputs, outputs)
         data_loader = DataLoader(dataset, batch_size=batch, shuffle=shuffle)
         return data_loader, inputs, outputs
--- utils/word2vec.py
+++ utils/word2vec.py
@@ -11,13 +11,12 @@ import torch
 from tqdm import tqdm
 
 
-"""
-A great implementation of Word2vec is offered by Gensim.
-You can use pre-trained models or train your own model.
-"""
-
-
 class Word2VecDataset(Dataset):
+    """
+    Dataset class to iterate over a corpus of data which has been previously
+    processed using SkipWords (so the pairs <central_word, surrounding_context>
+    """
+
     def __init__(self, data):
         self.data = data
 
@@ -32,28 +31,35 @@ class Word2VecDataset(Dataset):
 class Word2VecModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
         super(Word2VecModel, self).__init__()
-        # Init embedding layers
+        # Input layer of the Word2Vec network
         self.in_embedding = nn.Parameter(torch.randn(vocab_size, embedding_dim))
+        # Output layer of the Word2Vec network
         self.out_embedding = nn.Parameter(torch.randn(vocab_size, embedding_dim))
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = torch.optim.SGD(self.parameters(), lr=0.01)
 
     def forward(self, center_word_idx):
-        # Get embedding (manual lookup)
-        center_embedding = self.in_embedding[center_word_idx]  # (batch_size, embedding_dim)
-        scores = torch.matmul(center_embedding, self.out_embedding.T)  # (batch_size, vocab_size)
+        # Forward = run the input through the network and return output
+        center_embedding = self.in_embedding[center_word_idx]  # Get the embedding
+        scores = torch.matmul(center_embedding, self.out_embedding.T)  # Turn into scores
         return scores
 
-    def train(self, dataloader, n_epochs: int, step: float = 0.1):
-        history = []
-        check = int(n_epochs * step)
+    def train(
+            self,
+            dataloader,
+            n_epochs: int,
+            step: float = 0.1
+    ) -> list[float]:
+        """Performs the training, returns the loss every (n_epochs * step) iterations"""
+        loss_history = []
+        loss_checkpoint = int(n_epochs * step)
         for epoch in range(n_epochs):
             total_loss = 0
             for center, context in dataloader:
                 # Gradient reset
                 self.optimizer.zero_grad()
                 # Forward pass
-                output = self(center)
+                output = self(center)  # Calls self.forward(center)
                 # Loss
                 loss = self.criterion(output, context)
                 # Backpropagation
@@ -61,26 +67,35 @@ class Word2VecModel(nn.Module):
                 # Optimization
                 self.optimizer.step()
                 total_loss += loss.item()
-            if epoch % check == 0:
-                history.append(total_loss)
-        return history
+            if epoch % loss_checkpoint == 0:
+                loss_history.append(total_loss)
+        return loss_history
 
     @property
     def embeddings(self):
+        """The embeddings learnt by the network"""
         return self.in_embedding.data
 
-    def predict_context_words(self, word, vocabulary, top_k=10):
-        # Converti la parola in indice
+    def predict_context_words(
+            self,
+            word,
+            vocabulary,
+            top_k=10
+    ):
+        # Find word index
         word_idx = vocabulary.word2idx([word])[0]
         word_tensor = torch.tensor([word_idx], dtype=torch.long)
-        # Use the model to get predictions
+
+        # Compute output of the network (apply softmax)
         with torch.no_grad():
             output_scores = self(word_tensor)  # (1, vocab_size)
-        probs = F.softmax(output_scores, dim=1)  # (1, vocab_size)
-        # Get top probabilities for words
+        probs = F.softmax(output_scores, dim=1)  # Turns scores into a probability distribution
+
+        # Select top-k probabilities
         top_probs, top_indices = torch.topk(probs, top_k)
         top_indices = top_indices[0].tolist()
-        # Indeces to words
+
+        # Turn indices into words lists
         predicted_words = vocabulary.idx2tokens(top_indices)
         predicted_probs = top_probs[0].tolist()
         return predicted_words, predicted_probs
@@ -124,7 +139,6 @@ class Word2WordPrediction(nn.Module):
     def _embeddings(self):
         weights_fc1 = self.fc1.weight.data.detach().numpy()
         self.vectors = weights_fc1.T
-
 
 
 class WordEmbeddings:
@@ -188,4 +202,3 @@ class WordEmbeddings:
         word_v = self.vectors(words=words)
         center_v = word_v.mean(axis=0)
         return self.vector_similarity(center_v, topk=topk)
-
